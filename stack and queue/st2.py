f43b8875b73fde6c92a91fe41b465872f8409bc8
Rushi6767 <rushisathavara581998@gmail.com>
2025-06-03 21:26:24 -0400

    Understanding deque

---
+++ stack and queue/st2.py
@@ -0,0 +1,17 @@
+"""
+Understanding deque
+"""
+from collections import deque
+
+lst = deque([])
+lst.append(10)
+lst.append(20)
+lst.append(30)
+
+lst.appendleft(5)
+lst.appendleft(2)
+print(lst)
+
+lst.pop()
+lst.popleft()
+print(lst)
