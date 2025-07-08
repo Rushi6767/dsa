dba2f48f694354165457c1084f19ee4b9c139bf3
Rushi6767 <rushisathavara581998@gmail.com>
2025-07-08 13:40:01 -0400

    Floyd Warshall Algorithm

---
+++ graphs/gra35.py
@@ -0,0 +1,21 @@
+"""
+Floyd Warshall Algorithm (All-Pairs Shortest Path)
+"""
+class Solution:
+    def floydWarshall(self, dist):
+        n = len(dist)
+        for via in range(n):
+            for i in range(n):
+                for j in range(n):
+                    if dist[i][via] != 10**8 and dist[via][j] != 10**8:
+                        dist[i][j] = min(dist[i][j], dist[i][via] + dist[via][j])                
+        return dist
+    
+dist = [[0, 4, 10**8, 5, 10**8], [10**8, 0, 1, 10**8, 6], [2, 10**8, 0, 3, 10**8], [10**8, 10**8, 1, 0, 2], [1, 10**8, 10**8, 4, 0]]
+S=Solution()
+print(S.floydWarshall(dist))
+
+"""
+Time complexity : O(n**3)
+Space complexity : O(n**2)
+"""
